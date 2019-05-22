980e2f86ac0b4cfcbb2e58eaef09f32062fab96b
gjovic1 <gjovic1@etf.unsa.ba>
2019-05-22 09:59:29 +0200

    Obavljene neke prepravke.

--- src/ba/unsa/etf/rs/tutorijal8/TransportDAO.java
+++ src/ba/unsa/etf/rs/tutorijal8/TransportDAO.java
@@ -1,17 +1,20 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.BitSet;
 
 public class TransportDAO {
     private static TransportDAO instance = null;
-    private static Connection conn; 
+    private static Connection conn;
 
-    private PreparedStatement addDriver, zadnjiVozacId,
+    private PreparedStatement addDriver, zadnjiVozacId;
     private TransportDAO() {
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:proba.db");
@@ -25,19 +28,19 @@ public class TransportDAO {
     }
 
     public static TransportDAO getInstance() {
+        if(instance==null) instance=new TransportDAO();
+        return instance;
     }
 
     private void setupDatabase() {
         String sql="";
-        URL x = getClass().getResource("/setupDatabase.sql");
+        URL x = getClass().getResource("/dbScript.sql");
         FileReader fileReader = null;
 
         try {
-
             fileReader = new FileReader(x.getFile());
-
-        } catch (SQLException e) {
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
@@ -46,5 +49,26 @@ public class TransportDAO {
 
 
     public void resetDatabase() {
+    }
+
+    public ArrayList<Driver> getDrivers() {
+    }
+
+    public void addBus(Bus bus) {
+    }
+
+    public BitSet getBusses() {
+    }
+
+    public void deleteBus(Bus bus) {
+    }
+
+    public void addDriver(Driver driver) {
+    }
+
+    public void deleteDriver(Driver driver) {
+    }
+
+    public void dodijeliVozacuAutobus(Driver driver, Bus bus, int which) {
     }
 }
